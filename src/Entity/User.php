44276f9ec5d861f4082a7b9ee475712faae90ef6
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-10 20:01:07 +0400

    First Class Entity\User

---
+++ src/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry @ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Common;
+
+/**
+ * Class User
+ * @package SocialConnect\Common
+ */
+class User
+{
+    /**
+     * @var string
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $firstname;
+
+    /**
+     * @var string
+     */
+    public $lastname;
+}
