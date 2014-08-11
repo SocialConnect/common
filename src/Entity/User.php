700ed3a9036e915dea9c252d3d27b2d4713d39c3
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-11 06:28:55 +0400

    Fix namespace

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,11 @@
  * @author: Patsura Dmitry @ovr <talk@dmtry.me>
  */
 
-namespace SocialConnect\Common;
+namespace SocialConnect\Common\Entity;
 
 /**
  * Class User
- * @package SocialConnect\Common
+ * @package SocialConnect\Common\Entity
  */
 class User
 {
