5db656831273a32594cecc984d017cfe596571b7
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-18 01:04:34 +0400

    Add CloneObjectMap hydrator

---
+++ src/Hydrator/CloneObjectMap.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry @ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Common\Hydrator;
+
+/**
+ * Class ObjectMap
+ * @package SocialConnect\Common\Hydrator
+ */
+class CloneObjectMap
+{
+    /**
+     * Hydration map
+     *
+     * @var array
+     */
+    protected $map;
+
+    /**
+     * @var object
+     */
+    protected $instance;
+
+    /**
+     * @param array $map
+     * @param object $instance
+     */
+    public function __construct(array $map, $instance)
+    {
+        $this->map = $map;
+        $this->instance = $instance;
+    }
+
+    /**
+     * Hydrate $targetObject
+     *
+     * @param $inputObject
+     * @return object
+     */
+    public function hydrate($inputObject)
+    {
+        $result = clone $this->instance;
+
+        foreach(get_object_vars($inputObject) as $key => $value) {
+            $result->{$this->map[$key]} = $value;
+        }
+
+        return $result;
+    }
+}
