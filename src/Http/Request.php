15f36210ddc171b5c5adea5cd32fc2a3f12e2b0a
Dmitry Patsura <talk@dmtry.me>
2015-09-29 11:10:51 +0300

    Implement Http\Request

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,7 +6,60 @@
 
 namespace SocialConnect\Common\Http;
 
+use SocialConnect\Common\Http\Client\Client;
+
 class Request
 {
+    /**
+     * @var string
+     */
+    protected $uri;
 
+    /**
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @var array
+     */
+    protected $parameters;
+
+    /**
+     * Request constructor.
+     *
+     * @param string $uri
+     * @param array $parameters
+     * @param string $method
+     */
+    public function __construct($uri, array $parameters = array(), $method = Client::GET)
+    {
+        $this->uri = $uri;
+        $this->method = $method;
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
 }
